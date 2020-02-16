f86b4796f0229a233b502cb5704bc098ef717c71
cagri <cagri.uysal@semtr.com>
2020-02-16 23:29:15 +0300

    import data from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import courseNames from '../../backend/sekizkirk_cache/course_names.json';
+import courseSlots from '../../backend/sekizkirk_cache/course_slots.json';
+import courseCodes from '../../backend/sekizkirk_cache/course_codes.json';
+
 ReactDOM.render(<h1>Senpai is Awesome!</h1>, document.getElementById('root'));
