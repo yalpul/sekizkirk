959ce85185aec5f290c1e0f40d38b33db682c189
cagri <cagri.uysal07@gmail.com>
2020-06-12 01:05:24 +0300

    material ui added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 import LandingPage from './components/LandingPage';
 
 const App = () => {
-    return <LandingPage />;
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <LandingPage />
+        </React.Fragment>
+    )
 };
 
 export default App;
