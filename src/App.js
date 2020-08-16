34b74462962c896b40641e9042acc0bf949b54d6
cagri <cagri.uysal07@gmail.com>
2020-08-16 22:12:58 +0300

    Landing page headers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core';
+
 
 import LandingPage from './components/LandingPage';
+import theme from './components/Theme'
+
 
 const App = () => {
     return (
-        <React.Fragment>
+        <ThemeProvider theme={theme}>
             <CssBaseline />
-            <LandingPage />
-        </React.Fragment>
+            <main>
+                <LandingPage />
+                <div>
+                    User forms will be here
+                </div>
+            </main>
+        </ThemeProvider>
     )
 };
 
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,12 +1,57 @@
 import React, { useContext } from 'react';
 
+import { makeStyles, Button } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+
 import DataContext from './DataContext';
 
-const LandingPage = () => {
-    const data = useContext(DataContext);
-    console.log(data);
+import Background from '../assets/landing-background'
 
-    return <h1>LandingPage</h1>;
+import '../index.css'
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        backgroundColor: theme.palette.common.black,
+        height: '100vh',
+        display: 'flex',
+        color: theme.palette.common.white,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexFlow: 'column wrap',
+    },
+    bottomOffset: {
+        position: 'relative',
+        bottom: '80px',
+    },
+    svgWrapper: {
+        width: '120vmax',
+        margin: '0',
+        padding: '0',
+        border: '0',
+        // positioning absolute for using svg as background.
+        // NOTE: better practice might exist here.
+        position: 'absolute',
+    }
+}))
+
+const LandingPage = () => {
+    const classes = useStyles();
+
+    return (
+        <article className={classes.root}>
+            <figure className={classes.svgWrapper}>
+                {/* TODO: change this way importing of the svg with more proper practice */}
+                <Background />
+            </figure>
+            <Typography variant="h1" >
+                sekiz
+            </Typography>
+            <Typography variant="h1" className={classes.bottomOffset}>
+                kırk
+            </Typography>
+        </article >
+    )
 };
 
 export default LandingPage;
