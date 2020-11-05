db944da341d9032aa5ae2e6ea3698c27915b9832
cagri <cagri.uysal07@gmail.com>
2020-11-06 00:13:46 +0300

    email model added

--- backend/sekizkirk/email_handler/models.py
+++ backend/sekizkirk/email_handler/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Email(models.Model):
+    email_name = models.EmailField()
+    schedule = models.JSONField()
--- backend/sekizkirk/sekizkirk/settings.py
+++ backend/sekizkirk/sekizkirk/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'email_handler.apps.EmailHandlerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
