4419fb9e6203ea934ded20c798046c71e5493e17
cagri <cagri.uysal07@gmail.com>
2020-11-14 13:47:52 +0300

    must course addition functionalty moved to AddMusts

---
+++ src/components/AddMusts.js
@@ -0,0 +1,154 @@
+import React, { useContext } from "react";
+
+import DataContext from "./DataContext";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import Typography from "@material-ui/core/Typography";
+import FormControl from "@material-ui/core/FormControl";
+
+const useStyles = makeStyles((theme) => ({
+    accordionContainer: {
+        marginTop: "2em",
+    },
+    accordion: {
+        backgroundColor: "inherit",
+        border: `1px solid ${theme.palette.common.black}`,
+        [theme.breakpoints.up("sm")]: {
+            width: "35em",
+        },
+        [theme.breakpoints.down("xs")]: {
+            width: "20em",
+        },
+    },
+    accordionSummary: {
+        "& .MuiAccordionSummary-content": {
+            alignItems: "center",
+            flexGrow: 0,
+        },
+    },
+    deptSelect: {
+        [theme.breakpoints.down("xs")]: {
+            marginBottom: theme.spacing(2),
+        },
+    },
+    mustSelect: {
+        minWidth: "200px",
+    },
+}));
+
+const AddMusts = ({ setDept, setSemester, semester }) => {
+    const classes = useStyles();
+    const data = useContext(DataContext);
+
+    const handleDepartmentChange = (event, value) => {
+        setDept(value);
+    };
+
+    const handleSemesterChange = (event) => {
+        setSemester(event.target.value);
+    };
+
+    console.log("AddMusts rendered.");
+    return (
+        <Grid item className={classes.accordionContainer}>
+            <Accordion className={classes.accordion}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                    className={classes.accordionSummary}
+                >
+                    <Typography variant="body1">Add Must Courses</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Grid container justify="space-around" alignItems="center">
+                        <Grid item className={classes.deptSelect}>
+                            <Autocomplete
+                                options={data.departments}
+                                getOptionLabel={(department) =>
+                                    department.title
+                                }
+                                renderInput={(params) => (
+                                    <TextField
+                                        {...params}
+                                        label="Department"
+                                        variant="outlined"
+                                    />
+                                )}
+                                onChange={handleDepartmentChange}
+                                popupIcon={<></>} // no icon
+                                fullWidth
+                                clearOnEscape
+                                autoSelect
+                                blurOnSelect
+                                autoHighlight
+                                noOptionsText="Not found."
+                                className={classes.mustSelect}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <FormControl
+                                variant="outlined"
+                                className={classes.mustSelect}
+                            >
+                                <InputLabel id="semester-select">
+                                    Semester
+                                </InputLabel>
+                                <Select
+                                    labelId="semester-select"
+                                    value={semester}
+                                    onChange={handleSemesterChange}
+                                    label="semester"
+                                >
+                                    <ListSubheader
+                                        style={{
+                                            fontWeight: 700,
+                                            pointerEvents: "none",
+                                        }}
+                                    >
+                                        Fall
+                                    </ListSubheader>
+                                    {[1, 3, 5, 7].map((sem) => (
+                                        <MenuItem key={sem} value={sem}>
+                                            {sem}
+                                        </MenuItem>
+                                    ))}
+                                    <ListSubheader
+                                        style={{
+                                            fontWeight: 700,
+                                            pointerEvents: "none",
+                                        }}
+                                    >
+                                        Spring
+                                    </ListSubheader>
+                                    {[2, 4, 6, 8].map((sem) => (
+                                        <MenuItem
+                                            key={sem}
+                                            value={sem}
+                                            disabled
+                                        >
+                                            {sem}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                    </Grid>
+                </AccordionDetails>
+            </Accordion>
+        </Grid>
+    );
+};
+
+export default React.memo(AddMusts);
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,12 @@
 import React, { useState, useEffect, useContext } from "react";
+
 import DataContext from "./DataContext";
+import AddCourse from "./AddCourse";
+import AddMusts from "./AddMusts";
 
 import { makeStyles } from "@material-ui/core/styles";
-
-import AddCourse from "./AddCourse";
-
 import Grid from "@material-ui/core/Grid";
-import TextField from "@material-ui/core/TextField";
-import Autocomplete from "@material-ui/lab/Autocomplete";
 import Typography from "@material-ui/core/Typography";
-import Accordion from "@material-ui/core/Accordion";
-import AccordionDetails from "@material-ui/core/AccordionDetails";
-import AccordionSummary from "@material-ui/core/AccordionSummary";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
-import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
@@ -29,7 +20,6 @@ import ClearAllIcon from "@material-ui/icons/ClearAll";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
-import ListSubheader from "@material-ui/core/ListSubheader";
 import SchoolIcon from "@material-ui/icons/School";
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
@@ -60,33 +50,6 @@ const useStyles = makeStyles((theme) => ({
         [theme.breakpoints.down("xs")]: {
             fontSize: "2.5em",
         },
-    },
-    accordionContainer: {
-        marginTop: "2em",
-    },
-    accordion: {
-        backgroundColor: "inherit",
-        border: `1px solid ${theme.palette.common.black}`,
-        [theme.breakpoints.up("sm")]: {
-            width: "35em",
-        },
-        [theme.breakpoints.down("xs")]: {
-            width: "20em",
-        },
-    },
-    accordionSummary: {
-        "& .MuiAccordionSummary-content": {
-            alignItems: "center",
-            flexGrow: 0,
-        },
-    },
-    deptSelect: {
-        [theme.breakpoints.down("xs")]: {
-            marginBottom: theme.spacing(2),
-        },
-    },
-    mustSelect: {
-        minWidth: "200px",
     },
     courseListContainer: {
         width: "100%",
@@ -161,14 +124,6 @@ const Form = ({
         if (courseValue !== null && !courses.includes(courseValue)) {
             setManualCourses(manualCourses.concat(courseValue));
         }
-    };
-
-    const handleDepartmentChange = (event, value) => {
-        setDept(value);
-    };
-
-    const handleSemesterChange = (event) => {
-        setSemester(event.target.value);
     };
 
     const handleClearAll = () => {
@@ -289,99 +244,12 @@ const Form = ({
                 handleCourseAdd={handleCourseAdd}
             />
 
-            <Grid item className={classes.accordionContainer}>
-                <Accordion className={classes.accordion}>
-                    <AccordionSummary
-                        expandIcon={<ExpandMoreIcon />}
-                        aria-controls="panel1a-content"
-                        id="panel1a-header"
-                        className={classes.accordionSummary}
-                    >
-                        <Typography variant="body1">
-                            Add Must Courses
-                        </Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Grid
-                            container
-                            justify="space-around"
-                            alignItems="center"
-                        >
-                            <Grid item className={classes.deptSelect}>
-                                <Autocomplete
-                                    options={data.departments}
-                                    getOptionLabel={(department) =>
-                                        department.title
-                                    }
-                                    renderInput={(params) => (
-                                        <TextField
-                                            {...params}
-                                            label="Department"
-                                            variant="outlined"
-                                        />
-                                    )}
-                                    onChange={handleDepartmentChange}
-                                    popupIcon={<></>} // no icon
-                                    fullWidth
-                                    clearOnEscape
-                                    autoSelect
-                                    blurOnSelect
-                                    autoHighlight
-                                    noOptionsText="Not found."
-                                    className={classes.mustSelect}
-                                />
-                            </Grid>
-                            <Grid item>
-                                <FormControl
-                                    variant="outlined"
-                                    className={classes.mustSelect}
-                                >
-                                    <InputLabel id="semester-select">
-                                        Semester
-                                    </InputLabel>
-                                    <Select
-                                        labelId="semester-select"
-                                        value={semester}
-                                        onChange={handleSemesterChange}
-                                        label="semester"
-                                    >
-                                        <ListSubheader
-                                            style={{
-                                                fontWeight: 700,
-                                                pointerEvents: "none",
-                                            }}
-                                        >
-                                            Fall
-                                        </ListSubheader>
-                                        {[1, 3, 5, 7].map((sem) => (
-                                            <MenuItem key={sem} value={sem}>
-                                                {sem}
-                                            </MenuItem>
-                                        ))}
-                                        <ListSubheader
-                                            style={{
-                                                fontWeight: 700,
-                                                pointerEvents: "none",
-                                            }}
-                                        >
-                                            Spring
-                                        </ListSubheader>
-                                        {[2, 4, 6, 8].map((sem) => (
-                                            <MenuItem
-                                                key={sem}
-                                                value={sem}
-                                                disabled
-                                            >
-                                                {sem}
-                                            </MenuItem>
-                                        ))}
-                                    </Select>
-                                </FormControl>
-                            </Grid>
-                        </Grid>
-                    </AccordionDetails>
-                </Accordion>
-            </Grid>
+            <AddMusts
+                setDept={setDept}
+                setSemester={setSemester}
+                semester={semester}
+            />
+
             <Grid item className={classes.courseListContainer}>
                 {courses.length > 0 && (
                     <>
