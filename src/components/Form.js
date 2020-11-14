86cb47fc41b074a0dd26579bc7b71ee08adcb2a5
cagri <cagri.uysal07@gmail.com>
2020-11-14 18:19:06 +0300

    selective list functionalty moved to SelectiveList

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,18 +4,13 @@ import DataContext from "./DataContext";
 import AddCourse from "./AddCourse";
 import AddMusts from "./AddMusts";
 import CoursesList from "./CoursesList";
+import SelectiveList from "./SelectiveList";
 
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import FormControl from "@material-ui/core/FormControl";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import Divider from "@material-ui/core/Divider";
 import Button from "@material-ui/core/Button";
-import CheckCircleIcon from "@material-ui/icons/CheckCircle";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
 import SchoolIcon from "@material-ui/icons/School";
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
@@ -45,24 +40,6 @@ const useStyles = makeStyles((theme) => ({
     heading: {
         [theme.breakpoints.down("xs")]: {
             fontSize: "2.5em",
-        },
-    },
-    selectiveContainer: {
-        width: "100%",
-        maxWidth: "35em",
-        marginTop: "1em",
-        backgroundColor: theme.palette.common.listBackground,
-    },
-    selectiveHeader: {
-        fontWeight: 700,
-        fontFamily: "Agrandir",
-        paddingLeft: theme.spacing(2),
-        paddingTop: theme.spacing(1),
-        paddingBottom: theme.spacing(1),
-    },
-    listItem: {
-        "&:hover .list-icon": {
-            color: theme.palette.primary.dark,
         },
     },
     buttonContainer: {
@@ -111,11 +88,6 @@ const Form = ({
         if (courseValue !== null && !courses.includes(courseValue)) {
             setManualCourses(manualCourses.concat(courseValue));
         }
-    };
-
-    const handleSelectiveClick = (course) => {
-        setMustCourses([...mustCourses].concat(course));
-        setSelectiveCourses([]);
     };
 
     const handleScheduleClick = () => {
@@ -222,39 +194,13 @@ const Form = ({
                 setOpenDialog={setOpenDialog}
             />
 
-            <Grid item className={classes.selectiveContainer}>
-                {selectiveCourses.length > 0 && (
-                    <>
-                        <Typography
-                            variant="subtitle1"
-                            className={classes.selectiveHeader}
-                            color="secondary"
-                        >
-                            Selectives for this semester
-                        </Typography>
-                        <Divider />
-                        <List dense>
-                            {selectiveCourses.map((course) => {
-                                return course ? (
-                                    <ListItem
-                                        key={`${course.title}+${course.code}`}
-                                        button
-                                        onClick={() =>
-                                            handleSelectiveClick(course)
-                                        }
-                                        className={classes.listItem}
-                                    >
-                                        <ListItemIcon>
-                                            <CheckCircleIcon className="list-icon" />
-                                        </ListItemIcon>
-                                        <ListItemText primary={course.title} />
-                                    </ListItem>
-                                ) : null;
-                            })}
-                        </List>
-                    </>
-                )}
-            </Grid>
+            <SelectiveList
+                selectiveCourses={selectiveCourses}
+                setMustCourses={setMustCourses}
+                mustCourses={mustCourses}
+                setSelectiveCourses={setSelectiveCourses}
+            />
+
             <Grid item className={classes.buttonContainer}>
                 {courses.length > 0 && (
                     <Button
