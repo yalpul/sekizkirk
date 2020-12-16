33a47ce634e290663828523a3edb800c30c306d2
cagri <cagri.uysal07@gmail.com>
2020-12-16 23:47:21 +0300

    chips UI for instructors

--- src/components/Form/SectionOptions.js
+++ src/components/Form/SectionOptions.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import { DataContext } from "../DataContext";
 import {
@@ -8,6 +8,7 @@ import {
     TOGGLE_COLLISION,
 } from "../CoursesContext";
 
+import { useTheme, makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
@@ -24,13 +25,31 @@ import Button from "@material-ui/core/Button";
 import FilterListIcon from "@material-ui/icons/FilterList";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
+import Chip from "@material-ui/core/Chip";
+
+const useStyles = makeStyles((theme) => ({
+    chipContainer: {
+        marginTop: "0.5em",
+        marginBottom: "0.5em",
+    },
+    chip: {
+        color: "#fff",
+        marginRight: "0.5em",
+        marginBottom: "0.5em",
+    },
+}));
 
 const SectionOptions = ({ index, course, openDialog, setOpenDialog }) => {
+    const classes = useStyles();
+    const theme = useTheme();
+
     const { courseSlots } = useContext(DataContext);
     const { coursesState, dispatch } = useContext(CoursesContext);
     const { sectionChecks, allowCollision, fixedSections } = coursesState;
 
+    const [instructors, setInstuctors] = useState([]);
     const [showInstructors, setShowInstructors] = useState(false);
+    const [instructorActive, setInstructorActive] = useState([]);
 
     const handleUnselectAll = (course) => {
         dispatch({ type: UNSELECT_ALL_SECTIONS, payload: { course } });
@@ -43,6 +62,30 @@ const SectionOptions = ({ index, course, openDialog, setOpenDialog }) => {
     const handleCollisionCheck = (course) => {
         dispatch({ type: TOGGLE_COLLISION, payload: { course } });
     };
+
+    const handleInstructorClick = (index) => {
+        const temp = [...instructorActive];
+        temp[index] = !temp[index];
+        setInstructorActive(temp);
+    };
+
+    useEffect(() => {
+        const getInstructors = () => {
+            // unique instructors
+            return [
+                ...new Set(
+                    courseSlots[course.code].map((slot) => {
+                        const [, , , instructorName] = slot;
+                        return instructorName;
+                    })
+                ),
+            ];
+        };
+
+        const instructors = getInstructors();
+        setInstuctors(instructors);
+        setInstructorActive(instructors.map(() => true));
+    }, []);
 
     return (
         <Dialog
@@ -85,63 +128,108 @@ const SectionOptions = ({ index, course, openDialog, setOpenDialog }) => {
                     <Grid item style={{ marginTop: "1em" }}>
                         <FormControl component="fieldset">
                             <FormLabel component="legend" focused={false}>
-                                <Grid container alignItems="center">
+                                <Grid container direction="column">
                                     <Grid item>
-                                        <Typography>Course Sections</Typography>
-                                    </Grid>
-                                    <Grid
-                                        item
-                                        style={{
-                                            marginLeft: "0.5em",
-                                            marginRight: "0.5em",
-                                        }}
-                                    >
-                                        <Button
-                                            variant="outlined"
-                                            size="small"
-                                            color="secondary"
-                                            onClick={() =>
-                                                handleUnselectAll(course)
-                                            }
-                                            disabled={
-                                                fixedSections[course.code]
-                                            }
-                                        >
-                                            Unselect All
-                                        </Button>
-                                    </Grid>
+                                        <Grid container alignItems="center">
+                                            <Grid item>
+                                                <Typography>
+                                                    Course Sections
+                                                </Typography>
+                                            </Grid>
 
-                                    <Grid item>
-                                        {!fixedSections[course.code] && (
-                                            <Tooltip
-                                                title="filter by instructor"
-                                                arrow
-                                                enterDelay={500}
+                                            <Grid
+                                                item
+                                                style={{
+                                                    marginLeft: "0.5em",
+                                                    marginRight: "0.5em",
+                                                }}
                                             >
-                                                <IconButton
-                                                    aria-label="filter"
+                                                <Button
+                                                    variant="outlined"
+                                                    size="small"
                                                     color="secondary"
                                                     onClick={() =>
-                                                        setShowInstructors(
-                                                            !showInstructors
+                                                        handleUnselectAll(
+                                                            course
                                                         )
                                                     }
+                                                    disabled={
+                                                        fixedSections[
+                                                            course.code
+                                                        ]
+                                                    }
                                                 >
-                                                    <FilterListIcon />
-                                                </IconButton>
-                                            </Tooltip>
-                                        )}
+                                                    Unselect All
+                                                </Button>
+                                            </Grid>
+
+                                            <Grid item>
+                                                {!fixedSections[
+                                                    course.code
+                                                ] && (
+                                                    <Tooltip
+                                                        title="filter by instructor"
+                                                        arrow
+                                                        enterDelay={500}
+                                                    >
+                                                        <IconButton
+                                                            aria-label="filter"
+                                                            color="secondary"
+                                                            onClick={() =>
+                                                                setShowInstructors(
+                                                                    !showInstructors
+                                                                )
+                                                            }
+                                                        >
+                                                            <FilterListIcon />
+                                                        </IconButton>
+                                                    </Tooltip>
+                                                )}
+                                            </Grid>
+
+                                            <Grid item>
+                                                {fixedSections[course.code] && (
+                                                    <Tooltip
+                                                        arrow
+                                                        title="Unfix section in the table to enable selection."
+                                                    >
+                                                        <HelpIcon color="primary" />
+                                                    </Tooltip>
+                                                )}
+                                            </Grid>
+                                        </Grid>
                                     </Grid>
 
-                                    <Grid item>
-                                        {fixedSections[course.code] && (
-                                            <Tooltip
-                                                arrow
-                                                title="Unfix section in the table to enable selection."
-                                            >
-                                                <HelpIcon color="primary" />
-                                            </Tooltip>
-                                        )}
+                                    <Grid
+                                        item
+                                        className={classes.chipContainer}
+                                    >
+                                        {!fixedSections[course.code] &&
+                                            showInstructors &&
+                                            instructors.map(
+                                                (instructor, index) => (
+                                                    <Chip
+                                                        label={instructor}
+                                                        key={instructor}
+                                                        className={classes.chip}
+                                                        clickable
+                                                        style={{
+                                                            backgroundColor: instructorActive[
+                                                                index
+                                                            ]
+                                                                ? theme.palette
+                                                                      .primary
+                                                                      .light
+                                                                : undefined,
+                                                        }}
+                                                        onClick={() =>
+                                                            handleInstructorClick(
+                                                                index
+                                                            )
+                                                        }
+                                                    />
+                                                )
+                                            )}
                                     </Grid>
                                 </Grid>
                             </FormLabel>
